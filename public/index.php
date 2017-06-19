71dae66c56d8dd63f57e95e2b0f4f6607cccc3e5
Branden Garrett <branden.garrett@gmail.com>
2017-06-19 12:39:37 -0400

    updates public/index.php for Craft 2.6

--- public/index.php
+++ public/index.php
@@ -1,20 +1,23 @@
 <?php
-/**
- * Craft web bootstrap file
- */
-
-// Project root path
-$root = dirname(__DIR__);
-
-// Composer autoloader
-require_once $root.'/vendor/autoload.php';
 
 // CraftVM
-define('CRAFT_BASE_PATH', $root);
+
 define('CRAFT_ENVIRONMENT', getenv('CRAFT_ENV_ENVIRONMENT'));
 define('CRAFT_CONFIG_PATH', '../craft/config/');
 define('CRAFT_STORAGE_PATH', '../craft/storage/');
+define('CRAFT_TEMPLATES_PATH', '../templates/');
 
 // Craft
-$app = require $root.'/vendor/craftcms/cms/bootstrap/web.php';
-$app->run();
+$craftPath = '../craft';
+$path = rtrim($craftPath, '/').'/app/index.php';
+
+if (!is_file($path))
+{
+	if (function_exists('http_response_code'))
+	{
+		http_response_code(503);
+	}
+	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+}
+
+require_once $path;
